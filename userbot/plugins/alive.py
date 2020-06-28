6f316b291d4e34586210041f58bd5ccae8ff1b7e
blackg820 <blackg820@gmail.com>
2020-06-28 03:53:32 +0300

    Add files via upload

---
+++ userbot/plugins/alive.py
@@ -0,0 +1,28 @@
+"""Check if userbot alive. If you change these, you become the gayest gay such that even the gay world will disown you."""
+#IMG CREDITS: @WhySooSerious
+import asyncio
+from telethon import events
+from telethon.tl.types import ChannelParticipantsAdmins
+from platform import uname
+from userbot import ALIVE_NAME
+from userbot.utils import admin_cmd
+
+
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
+ALIVE_IMG = "https://blackroot.000webhostapp.com/dev0.jpg"
+ALIVE_caption = "`BLack IS:` **ONLINE**\n\n"
+ALIVE_caption += "**SYSTEM STATUS**\n\n"
+ALIVE_caption += "`TELETHON VERSION:` **6.0.9**\n`Python:` **3.7.4**\n\n"
+ALIVE_caption += "`DATABASE STATUS:` **Functional**\n\n"
+ALIVE_caption += "**Current Branch** : `master`\n\n"
+ALIVE_caption += "**BLack OS** : `3.14`\n\n"
+ALIVE_caption += "**Current Sat** : `e8mc-2.25`\n\n"
+ALIVE_caption += f"**My Boss** : {DEFAULTUSER} \n\n"
+ALIVE_caption += "**Heroku Database** : `AWS - Working Properly`\n\n"
+ALIVE_caption += "Copyright By [blackg820@Github](https://github.com/blackg820/e8mc)\n\n"
+#@command(outgoing=True, pattern="^.alive$")
+@borg.on(admin_cmd(pattern=r"alive"))
+async def amireallyalive(alive):
+    """ For .alive command, check if the bot is running.  """
+    await alive.delete() 
+    await borg.send_file(alive.chat_id, ALIVE_IMG,caption=ALIVE_caption)
