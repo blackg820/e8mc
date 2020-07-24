d1ed6a03998d3b800ae92505f674642e2326d034
blackg820 <blackg820@gmail.com>
2020-07-24 03:56:28 +0300

    Update heroku_config.py

--- heroku_config.py
+++ heroku_config.py
@@ -3,7 +3,7 @@ import os
 class Var(object):
     APP_ID = int(os.environ.get("APP_ID", 6))
     # 6 is a placeholder
-    API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+    API_HASH = os.environ.get("API_HASH", None)
     STRING_SESSION = os.environ.get("STRING_SESSION", None)
     DB_URI = os.environ.get("DATABASE_URL", None)
     TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", None)
@@ -11,11 +11,11 @@ class Var(object):
     GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
     GIT_REPO_NAME = os.environ.get("GIT_REPO_NAME", None)
     # Here for later purposes
-    SUDO_USERS = set(int(x) for x in os.environ.get("SUDO_USERS", "1097131648").split())
-    WHITELIST_USERS = set(int(x) for x in os.environ.get("WHITELIST_USERS", "832241419").split())
+    SUDO_USERS = set(int(x) for x in os.environ.get("SUDO_USERS", "264123112").split())
+    WHITELIST_USERS = set(int(x) for x in os.environ.get("WHITELIST_USERS", "264123112").split())
     BLACKLIST_USERS = set(int(x) for x in os.environ.get("BLACKLIST_USERS", "").split())
-    DEVLOPERS = set(int(x) for x in os.environ.get("DEVLOPERS", "719195224").split())
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "813878981").split())
+    DEVLOPERS = set(int(x) for x in os.environ.get("DEVLOPERS", "264123112").split())
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "264123112").split())
     SUPPORT_USERS = set(int(x) for x in os.environ.get("SUPPORT_USERS", "").split())
     PLUGIN_CHANNEL = int(os.environ.get("PLUGIN_CHANNEL", None))
     LYDIA_API_KEY = os.environ.get("LYDIA_API_KEY", None)
